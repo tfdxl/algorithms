e7673aa37d4f3cafa5e200fb672ab58c30006922
tianfeng <tianfeng@souche.com>
2017-03-26 19:47:38 +0800

    add

--- src/main/java/com/tfdxl/bst/BSTOpertation.java
+++ src/main/java/com/tfdxl/bst/BSTOpertation.java
@@ -7,12 +7,47 @@ public interface BSTOpertation {
 
     /**
      * 根据key查找
+     *
      * @param key
      * @return
      */
     WrappedData btreeSearch(int key);
 
+    /**
+     * 删除节点
+     *
+     * @param key
+     * @return
+     */
+    WrappedData delete(int key);
 
+    /**
+     * 获取key最小的节点
+     *
+     * @return
+     */
+    WrappedData treeMinimum();
 
+    /**
+     * 获取key最大的节点
+     *
+     * @return
+     */
+    WrappedData treeMaximum();
 
+    /**
+     * 获取x的前驱节点
+     *
+     * @param x
+     * @return
+     */
+    WrappedData treeSuccessor(WrappedData x);
+
+    /**
+     * 获取x的后继节点
+     *
+     * @param x
+     * @return
+     */
+    WrappedData treePredecessor(WrappedData x);
 }
--- src/main/java/com/tfdxl/bst/BinarySearchTree.java
+++ src/main/java/com/tfdxl/bst/BinarySearchTree.java
@@ -5,7 +5,78 @@ import lombok.Data;
 /**
  * Created by tianfeng on 2017/3/26.
  */
-public class BinarySearchTree {
+public class BinarySearchTree implements BSTOpertation {
+
+    private Node root;
+
+
+    @Override
+    public WrappedData btreeSearch(int key) {
+        return null;
+    }
+
+    @Override
+    public WrappedData delete(int key) {
+        Node node = searchBykey(key);
+        if(node.getLeft() == null){
+            transplant(node,node.getRight());
+        }else if(node.getRight() == null){
+            transplant(node,node.getLeft());
+        }else {
+
+        }
+        return node.getData();
+    }
+
+    @Override
+    public WrappedData treeMinimum() {
+        return null;
+    }
+
+    @Override
+    public WrappedData treeMaximum() {
+        return null;
+    }
+
+    @Override
+    public WrappedData treeSuccessor(WrappedData x) {
+        return null;
+    }
+
+    @Override
+    public WrappedData treePredecessor(WrappedData x) {
+        return null;
+    }
+
+
+
+    /**
+      用一颗以v为根的子树替换一个以u为根的子树
+     *
+     * @param u
+     * @param v
+     */
+    private void transplant(Node u, Node v) {
+        if (u.parent == null) {
+            this.root = u;
+        } else if (u == u.parent.getLeft()) {
+            u.getParent().setLeft(v);
+        } else {
+            u.getParent().setRight(v);
+        }
+        if (v != null) {
+            v.parent = u.parent;
+        }
+    }
+
+    /**
+     * 根据key查找节点
+     * @param key
+     * @return
+     */
+    private Node searchBykey(int key){
+        return null;
+    }
 
 
     /**
