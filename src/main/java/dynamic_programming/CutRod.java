1f07a41008e50f4214b01a69443abe7d9562780e
tianfeng <tianfeng@souche.com>
2017-04-04 03:08:15 +0800

    add

--- src/main/java/dynamic_programming/CutRod.java
+++ src/main/java/dynamic_programming/CutRod.java
@@ -99,6 +99,45 @@ public class CutRod {
         return temp[n];
     }
 
+
+    public int[][] extendedBottomUpCutRod(int[] weight, int n) {
+
+        if (n <= 0) {
+            return null;
+        }
+        //最优收益
+        int[] revenue = new int[n + 1];
+
+        //切割长度
+        int[] s = new int[n+1];
+
+        revenue[0] = 0;
+
+        for(int j=1;j<=n;j++){
+            int q = Integer.MIN_VALUE;
+            for(int i=1;i<=j;i++){
+                if(q<(weight[i]+revenue[j-i])){
+                    q = weight[i]+revenue[j-i];
+                    s[j] = i;
+                }
+                revenue[j] = q;
+            }
+        }
+        int[][] result = new int[2][n+1];
+        result[0] = revenue;
+        result[1] = s;
+        return result;
+    }
+
+    public void printCutRodSolution(int[] weight,int n){
+        int[][] revenueAndS = extendedBottomUpCutRod(weight,n);
+        int[] s = revenueAndS[1];
+        while(n>0){
+            System.out.println(s[n]);
+            n = n-s[n];
+        }
+    }
+
     public static void main(String[] args) {
 
         int[] weight = new int[40];
---
+++ src/main/java/dynamic_programming/Matrix.java
@@ -0,0 +1,29 @@
+package dynamic_programming;
+
+import lombok.Data;
+
+/**
+ * Created by tianfeng on 2017/4/4.
+ */
+@Data
+public class Matrix {
+
+    //列
+    private int columns;
+
+    //行
+    private int rows;
+
+    //data
+    private int[][] data;
+
+    public void setCell(int x,int y,int value){
+
+        if(x<0||x>this.rows -1||y<0||y>this.columns-1){
+            throw new ArrayIndexOutOfBoundsException("超越边界");
+        }
+        this.data[x][y] = value;
+    }
+
+
+}
