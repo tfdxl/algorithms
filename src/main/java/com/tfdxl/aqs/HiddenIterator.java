db9c43396d8d5f513e0bac9ee2c4d872e158078f
tianfeng <tianfeng@souche.com>
2017-04-23 20:25:49 +0800

    add

--- src/main/java/com/tfdxl/aqs/HiddenIterator.java
+++ src/main/java/com/tfdxl/aqs/HiddenIterator.java
@@ -1,7 +1,30 @@
 package com.tfdxl.aqs;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 /**
  * Created by tianfeng on 2017/4/23.
  */
 public class HiddenIterator {
+
+    private final Set<Integer> set = new HashSet<>();
+
+    public synchronized void add(Integer i){
+        set.add(i);
+    }
+
+    public synchronized void remove(Integer i){
+        set.remove(i);
+    }
+
+    public void addTenThings(){
+        Random r = new Random();
+        for(int i =0;i<10;i++){
+            add(r.nextInt());
+        }
+
+        System.out.println("The ten elements is "+set);
+    }
 }
--- src/main/java/com/tfdxl/thinkinjava/ThreadLocalTest.java
+++ src/main/java/com/tfdxl/thinkinjava/ThreadLocalTest.java
@@ -1,7 +1,26 @@
 package com.tfdxl.thinkinjava;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by tianfeng on 2017/4/21.
  */
-public class ThreadLocalTest {
+public class ThreadLocalTest
+{
+    public static String DB_URL = "";
+    private static ThreadLocal<Connection> connectionHolder = new ThreadLocal<Connection>(){
+
+        @Override
+        protected Connection initialValue() {
+            try {
+                return DriverManager.getConnection(DB_URL);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    };
+
 }
