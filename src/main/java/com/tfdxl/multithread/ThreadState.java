de6afe7082f9d4ee698030a0b5bbd6f7f34f9b89
tianfeng <tianfeng@souche.com>
2017-06-13 11:32:54 +0800

    add

---
+++ src/main/java/com/tfdxl/multithread/ThreadState.java
@@ -0,0 +1,60 @@
+package com.tfdxl.multithread;
+
+/**
+ * Created by tianfeng on 2017/6/13.
+ */
+public class ThreadState {
+
+    public static void main(String[] args) {
+
+        new Thread(new TimeWaiting(),"TimeWaitingThread").start();
+        new Thread(new Waiting(),"WaitingThread").start();
+        new Thread(new Blocked(),"BlockedThread-1").start();
+        new Thread(new Blocked(),"BlockedThread-2").start();
+
+    }
+
+    public static class TimeWaiting implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static class Waiting implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (Waiting.class) {
+                    try {
+                        Waiting.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static class Blocked implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
