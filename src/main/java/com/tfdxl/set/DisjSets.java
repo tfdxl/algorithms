05326fd878ed53fbe95391ffa114295908eb3e85
tianfeng <tianfeng@souche.com>
2017-10-01 22:34:13 +0800

    add

--- src/main/java/com/tfdxl/set/DisjSets.java
+++ src/main/java/com/tfdxl/set/DisjSets.java
@@ -21,6 +21,23 @@ public class DisjSets {
         s[root2] = root1;
     }
 
+    //路径压缩
+    //路径压缩在find操作期间与用来执行union的方法无关
+    public int findInCompressionWay(int x){
+        if(s[x] <0){
+            return x;
+        }else{
+            return s[x] = findInCompressionWay(s[x]);
+        }
+
+    }
+
+
+
+    public boolean isInSameSet(int root1,int root2){
+        return find(root1) == find(root2);
+    }
+
     public void unionByRank(int root1, int root2) {
         if (s[root2] < s[root1]) {
             //root2 is deeper
