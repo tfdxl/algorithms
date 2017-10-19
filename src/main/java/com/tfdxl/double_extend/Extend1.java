8f0980758ce3d60121a3c8942abdb96636d2e3ae
tianfeng <tianfeng@souche.com>
2017-10-19 10:47:10 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/main/java/com/tfdxl/double_extend/Extend1.java
@@ -0,0 +1,7 @@
+package com.tfdxl.double_extend;
+
+/**
+ * Created by tianfeng on 2017/10/19.
+ */
+public class Extend1 {
+}
---
+++ src/main/java/com/tfdxl/double_extend/Parent.java
@@ -0,0 +1,8 @@
+package com.tfdxl.double_extend;
+
+/**
+ * Created by tianfeng on 2017/10/19.
+ */
+public interface Parent {
+    void baseMethod();
+}
