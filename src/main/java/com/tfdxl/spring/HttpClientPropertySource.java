4ce79f2f0f773dd823e0a98e00f4db6b4de9b216
tianfeng <tianfeng@souche.com>
2017-11-06 12:17:08 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/com/tfdxl/spring/HttpClientPropertySource.java
+++ src/main/java/com/tfdxl/spring/HttpClientPropertySource.java
@@ -1,15 +1,26 @@
 package com.tfdxl.spring;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.StandardEnvironment;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 /**
- * Created by tianfeng on 2017/11/3.
+ * @author tianfeng
+ * @date 2017/11/3
  * <p>
  * 属性源的适配器，对于一个任意类型的属性源进行包装
  */
 public class HttpClientPropertySource extends PropertySource<HttpClient> {
+
+    public static final String PROPERTY = "baidu.content";
 
     public HttpClientPropertySource(String name, HttpClient source) {
         super(name, source);
@@ -23,9 +34,22 @@ public class HttpClientPropertySource extends PropertySource<HttpClient> {
      */
     @Override
     public Object getProperty(String name) {
-        if (name.equals("baidu.content")) {
+        if (PROPERTY.equals(name)) {
+            //创建一个Get方法
             HttpGet get = new HttpGet("http://www.baidu.com");
+            try {
+                return IOUtils.toString(source.execute(get).getEntity().getContent(), Charset.defaultCharset());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        ConfigurableEnvironment environment = new StandardEnvironment();
+        CloseableHttpClient httpClient = HttpClients.custom().build();
+        environment.getPropertySources().addFirst(new HttpClientPropertySource("httpPropertySource", httpClient));
+        System.err.println(environment.getProperty(PROPERTY));
     }
 }
